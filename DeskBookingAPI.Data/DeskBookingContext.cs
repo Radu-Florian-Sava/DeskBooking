c2417c1f55d8825187c31e09de81fa87dea67db9
Radu-Florian-Sava <radu.sava@stud.ubbcluj.ro>
2022-07-11 13:53:52 +0300

    Initial commit, just before section 5

---
+++ DeskBookingAPI.Data/DeskBookingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskBookingAPI.Data
+{
+    public class DeskBookingContext : DbContext
+    {
+        public DeskBookingContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- DeskBookingAPI/Program.cs
+++ DeskBookingAPI/Program.cs
@@ -1,3 +1,6 @@
+using DeskBookingAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddDbContext<DeskBookingContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase"))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
